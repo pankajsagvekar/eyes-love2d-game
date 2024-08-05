ed368df700dea6e77415eadd0d5c9c73607ac130
Pankaj Sagvekar <pankajsagvekar167@gmail.com>
2024-08-05 14:22:53 +0530

    adding the colors

--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
 function love.load()
+    eyeX = 200
+    eyeY = 200
     
 end
 
@@ -9,5 +11,16 @@ function love.update()
 end
 
 function love.draw()
+    local function setColor(r, g, b, a)
+        love.graphics.setColor(love.math.colorFromBytes(r, g, b, a))
+    end
+    love.graphics.setBackgroundColor(255,255,255)
     love.graphics.print('Press ESC to quit')
+
+    --eye
+    setColor(200, 200, 200)
+    love.graphics.circle("fill", eyeX, eyeY, 50)
+    --pupil
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.circle("fill", 200, 200, 15)
 end
