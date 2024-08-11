58d45f5ca13064f56f7fa102eec5f6e78a410945
Pankaj Sagvekar <pankajsagvekar167@gmail.com>
2024-08-11 19:48:50 +0530

    put code into callable function

--- main.lua
+++ main.lua
@@ -8,37 +8,43 @@ function love.update()
         love.event.quit()
     end
 
-    distanceX = love.mouse.getX() - eyeX
-    distanceY = love.mouse.getY() - eyeY
-    angle = math.atan2(distanceY, distanceX)
-    distance = math.min(math.sqrt(distanceX^2 + distanceY^2), 30)
-    pupilX = eyeX + (math.cos(angle) * distance)
-    pupilY = eyeY + (math.sin(angle) * distance)
+
 end
 
 function love.draw()
-    local function setColor(r, g, b, a)
-        love.graphics.setColor(love.math.colorFromBytes(r, g, b, a))
+    function drawEye(eyeX, eyeY)
+        local distanceX = love.mouse.getX() - eyeX
+        local distanceY = love.mouse.getY() - eyeY
+        local angle = math.atan2(distanceY, distanceX)
+        local distance = math.min(math.sqrt(distanceX^2 + distanceY^2), 30)
+        local pupilX = eyeX + (math.cos(angle) * distance)
+        local pupilY = eyeY + (math.sin(angle) * distance)
+        local function setColor(r, g, b, a)
+            love.graphics.setColor(love.math.colorFromBytes(r, g, b, a))
+        end
+        love.graphics.setBackgroundColor(255, 255, 255)
+        love.graphics.print('Press ESC to quit', love.graphics.getWidth() / 2)
+    
+        --eye
+        setColor(200, 200, 200)
+        love.graphics.circle("fill", eyeX, eyeY, 50)
+    
+        --pupil
+        love.graphics.setColor(0, 0, 0)
+        love.graphics.circle("fill", pupilX, pupilY, 15)
+    
+        --distance
+        -- love.graphics.setColor(0, 0, 0)
+        -- love.graphics.print(table.concat({
+        --     'distance X: ' .. distanceX,
+        --     'distance Y: ' .. distanceY,
+        --     'distance: '..distance,
+        --     'angle: '..angle,
+        --     'cos(angle): '..math.cos(angle),
+        --     'sin(angle): '..math.sin(angle),
+        -- }, '\n'))
     end
-    love.graphics.setBackgroundColor(255, 255, 255)
-    love.graphics.print('Press ESC to quit', love.graphics.getWidth() / 2)
-
-    --eye
-    setColor(200, 200, 200)
-    love.graphics.circle("fill", eyeX, eyeY, 50)
-
-    --pupil
-    love.graphics.setColor(0, 0, 0)
-    love.graphics.circle("fill", pupilX, pupilY, 15)
-
-    --distance
-    love.graphics.setColor(0, 0, 0)
-    love.graphics.print(table.concat({
-        'distance X: ' .. distanceX,
-        'distance Y: ' .. distanceY,
-        'distance: '..distance,
-        'angle: '..angle,
-        'cos(angle): '..math.cos(angle),
-        'sin(angle): '..math.sin(angle),
-    }, '\n'))
+   
+    drawEye(200, 200)
+    drawEye(330, 200)
 end
